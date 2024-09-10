ae32180b632d3015eafbf3b5ee14c905fbc54a82
vishwanathr <vishwanath.reddy@remiges.tech>
2024-09-10 15:43:46 +0530

    Added search functionality to user list and updated the form validation

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,3 +1,36 @@
+// demo-1
+// import { Component, OnInit } from '@angular/core';
+// import { UserService } from '../../services/user.service';
+// import { User } from '../../models/user';
+
+// @Component({
+//   selector: 'app-user-list',
+//   templateUrl: './user-list.component.html',
+//   styleUrls: ['./user-list.component.css'],
+// })
+// export class UserListComponent implements OnInit {
+//   users: User[] = [];
+
+//   constructor(private userService: UserService) { }
+
+//   ngOnInit(): void {
+//     this.loadUsers();
+//   }
+
+//   loadUsers(): void {
+//     this.users = this.userService.getUsers();
+//   }
+
+//   deleteUser(id: number): void {
+//     this.userService.deleteUser(id);
+//     this.loadUsers();
+//   }
+// }
+
+
+
+// demo-2
+// user-list.component.ts
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { User } from '../../models/user';
@@ -9,19 +42,27 @@ import { User } from '../../models/user';
 })
 export class UserListComponent implements OnInit {
   users: User[] = [];
+  searchTerm: string = ''; // Search term variable
 
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
-    this.loadUsers();
+    this.users = this.userService.getUsers();
   }
 
-  loadUsers(): void {
-    this.users = this.userService.getUsers();
+  // Function to filter users based on the search term
+  filteredUsers(): User[] {
+    if (!this.searchTerm) {
+      return this.users; // If no search term, return all users
+    }
+    return this.users.filter((user) =>
+      user.name.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
+      user.email.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
   }
 
   deleteUser(id: number): void {
     this.userService.deleteUser(id);
-    this.loadUsers();
+    this.users = this.users.filter((user) => user.id !== id);
   }
 }
